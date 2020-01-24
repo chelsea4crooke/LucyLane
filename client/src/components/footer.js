f405f20bdbd4a8e17404721d35ce48c7c57c8cd0
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-23 19:00:50 -0500

    icons

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,36 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Row, Col } from "reactstrap";
-// import { FontAwesomeIcon } from 'react-fontawesome'
+// import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
+// import { Row, Col } from "reactstrap";
+
+
+
 
 function Footer() {
   return (
     <div className="footer">
-    <Row>
-        <Col>
-        <p className="footText">Photos by credit</p>
-        </Col>
-        <vr></vr>
-        <Col>
-        <p className="footText">lucylanebooking@gmail.com</p>
-        </Col>
-        <vr></vr>
-        <Col>
-        <Link 
-            className= "socialBtn"
-            to="https://www.facebook.com/pg/LucyLaneLove/events/?ref=page_internal" 
-            target="_blank" 
-            >Facebook </Link>
-            </Col>
-        <vr></vr>
-        <Col>
-        <Link 
-            className= "socialBtn"
-            to="https://www.instagram.com/lucy._.lane/?hl=en"
-            target="_blank" 
-            >Instagram</Link>
-        </Col>
-     </Row>
+        <p className="footText">Photos credit Andre Magalhaes</p>
+        {/* <SocialIcon <p className="footText">lucylanebooking@gmail.com</p> */}
+        <SocialIcon url="https://www.facebook.com/pg/LucyLaneLove/events/?ref=page_internal" target="_blank"/>
+
+        <SocialIcon url="https://www.instagram.com/lucy._.lane/?hl=en" target="_blank"/>
     </div>
 
   );
