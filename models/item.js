f7be5a7405adf562a4374b280ef9fbe2c1e97ba6
Miranda Jaroneski <miranda.jaroneski@gmail.com>
2020-01-25 10:25:03 -0500

    yup

--- models/item.js
+++ models/item.js
@@ -6,10 +6,7 @@ const ItemSchema = new Schema ({
         type: String,
         required: true
     },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+    
 });
 
 module.exports = Item = mongoose.model('item', ItemSchema);
