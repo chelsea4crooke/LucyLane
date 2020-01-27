52186a0415977a27157838586f94c9abd72f5738
Tony Waterfield <tonywaterfieldrva@gmail.com>
2020-01-26 21:46:48 -0500

    updated link on directions form to send to contact us page

--- client/src/components/directions/Directions.js
+++ client/src/components/directions/Directions.js
@@ -30,7 +30,7 @@ class Directions extends React.Component {
       <main className="directBody">
       <div className="container">
           {/* <h1 className="display-4">Lucy Lane Address Page</h1> */}
-            <p className="lead">We do not give out the Lucy Lane address publicly. It is our home after all. If you’d like to come to a show,fill out <a className="formLink" href="hi">this form</a> and we’ll send you a password. Once you receive the password, click the button below and you’ll be able to access the address and all directions 8:00am the day of the show. You will not be able to access it after the show is over and the password changes every show.</p>
+            <p className="lead">We do not give out the Lucy Lane address publicly. It is our home after all. If you’d like to come to a show,fill out <a className="formLink" href="form">this form</a> and we’ll send you a password. Once you receive the password, click the button below and you’ll be able to access the address and all directions 8:00am the day of the show. You will not be able to access it after the show is over and the password changes every show.</p>
               <button className="btn btn-primary btn-lg passwordBtn" to="../Hidden/Hidden.js" target="_blank" onClick={(e) => this.handleClick(e)}>Enter Protected Area</button>
               <br></br>
               </div>
