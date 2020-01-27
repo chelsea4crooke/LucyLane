1a03c9de33e8282bd5ab9a67818f4bd57663e84e
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-27 18:28:01 -0500

    last one failed

--- client/src/components/directions/Directions.js
+++ client/src/components/directions/Directions.js
@@ -28,12 +28,7 @@ class Directions extends React.Component {
       return (
       <main className="directBody">
       <div className="container">
-<<<<<<< HEAD
             <p className="lead">We do not give out the Lucy Lane address publicly. It is our home after all. If you’d like to come to a show,fill out <Link className="form" to="/form">this form</Link> and we’ll send you a password. Once you receive the password, click the button below and you’ll be able to access the address and all directions 8:00am the day of the show. You will not be able to access it after the show is over and the password changes every show.</p>
-=======
-          {/* <h1 className="display-4">Lucy Lane Address Page</h1> */}
-            <p className="lead">We do not give out the Lucy Lane address publicly. It is our home after all. If you’d like to come to a show,fill out <a className="formLink" href="form">this form</a> and we’ll send you a password. Once you receive the password, click the button below and you’ll be able to access the address and all directions 8:00am the day of the show. You will not be able to access it after the show is over and the password changes every show.</p>
->>>>>>> 52186a0415977a27157838586f94c9abd72f5738
               <button className="btn btn-primary btn-lg passwordBtn" to="../Hidden/Hidden.js" target="_blank" onClick={(e) => this.handleClick(e)}>Enter Protected Area</button>
               <br></br>
               </div>
