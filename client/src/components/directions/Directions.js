e5f7d3f9e3e3fd01132cdd4f2d6a033d6ea70211
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-27 18:45:36 -0500

    spacing test heroku

--- client/src/components/directions/Directions.js
+++ client/src/components/directions/Directions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 import { PropTypes } from 'prop-types';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./addressProtect.css"
 // import { Link } from 'react-router-dom';
 class Directions extends React.Component {
